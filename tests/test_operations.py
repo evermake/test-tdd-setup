20df3f2f6107bcc49fc184a04e572e1168a694b5
evermake <vladeryabkin@gmail.com>
2022-06-20 13:16:32 +0300

    tests: add operations basic tests

---
+++ tests/test_operations.py
@@ -0,0 +1,18 @@
+from app.opertaions import Sum, Subtract, Multiply, Divide
+
+def test_sum():
+    s = Sum()
+    assert s.execute(5, 123) == 128
+
+def test_subtract():
+    s = Subtract()
+    assert s.execute(5, 123) == -118
+
+def test_multiply():
+    m = Multiply()
+    assert m.execute(5, 123) == 615
+
+def test_divide():
+    d = Divide()
+    assert d.execute(50, 10) == 5
+
